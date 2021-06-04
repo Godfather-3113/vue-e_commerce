72e78c4de2245d5bf270e8ac342e9978b9c583a1
godfather <media.art.press2@gmail.com >
2021-06-04 16:58:17 +0500

    first_state

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="v-main-wrapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-main-wrapper"
+}
+</script>
+
+<style lang="scss">
+    .v-main-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      max-width: 900px;
+    }
+</style>
